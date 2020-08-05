f7929be2695e7bbb30424a10638f2dff51741c44
Matt Berns <bernstml19@gmail.com>
2020-08-05 11:42:46 -0500

    Updated get_catalog macro to 0.17.1

--- dbt/include/azuresynapse/macros/catalog.sql
+++ dbt/include/azuresynapse/macros/catalog.sql
@@ -1,10 +1,6 @@
-{% macro azuresynapse__get_catalog(information_schemas) -%}
+{% macro azuresynapse__get_catalog(information_schema, schemas) -%}
     {%- call statement('catalog', fetch_result=True) -%}
-      {% if (information_schemas | length) != 1 %}
-        {{ exceptions.raise_compiler_error('redshift get_catalog requires exactly one database') }}
-      {% endif %}
-        {% set database = information_schemas[0].database %}
-        {% set schema = information_schemas[0].schema %}
+        {% set database = information_schema.database %}
             with tables as (
                 select
                     table_catalog as "table_database",
@@ -14,23 +10,27 @@
                         when table_type = 'VIEW' then 'view'
                         else table_type
                     end as table_type
-                from {{database}}.{{schema}}.tables
+                from {{ database }}.information_schema.tables
+                where (
+                    {%- for schema in schemas -%}
+                    upper(table_schema) = upper('{{ schema }}'){%- if not loop.last %} or {% endif -%}
+                    {%- endfor -%}
+                )
             ),
             columns as (
                 select
                     table_catalog as "table_database",
                     table_schema as "table_schema",
                     table_name as "table_name",
-                    null as "table_comment",
                     column_name as "column_name",
                     ordinal_position as "column_index",
                     data_type as "column_type",
                     null as "column_comment"
-                from {{database}}.{{schema}}.columns
+                from {{ database }}.information_schema.columns
             )
-            select *
-            from tables
-            join columns on tables.table_database = columns.table_database and tables.table_schema = columns.table_schema and tables.table_name = columns.table_name
+            select DISTINCT t.table_database, t.table_schema, t.table_name, t.table_type, '' as table_comment, c.column_name, c.column_index, c.column_type, '' as column_comment
+            from tables t
+            join columns c on t.table_database = c.table_database and t.table_schema = c.table_schema and t.table_name = c.table_name
             order by "column_index"
   {%- endcall -%}
   {{ return(load_result('catalog').table) }}
