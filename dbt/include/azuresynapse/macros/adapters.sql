57d6b73caada984bb2b88e84060cbcd9f777ab1b
Han Hong <seung-han.hong@hotmail.com>
2020-09-14 18:07:05 +1000

    added a macro for check_cols=all

--- dbt/include/azuresynapse/macros/adapters.sql
+++ dbt/include/azuresynapse/macros/adapters.sql
@@ -161,3 +161,13 @@
   {%- endfor -%}
   ))
 {%- endmacro %}
+
+{% macro azuresynapse__get_columns_in_query(select_sql) %}
+    {% call statement('get_columns_in_query', fetch_result=True, auto_begin=False) -%}
+        select top 0 * from (
+            {{ select_sql }}
+        ) as __dbt_sbq
+    {% endcall %}
+
+    {{ return(load_result('get_columns_in_query').table.columns | map(attribute='name') | list) }}
+{% endmacro %}
