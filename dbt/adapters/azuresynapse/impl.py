3d7287f8721f28865cbec06a2ac079c71d36e85d
Matt Berns <56837350+mattberns@users.noreply.github.com>
2020-09-17 14:50:36 -0400

    Bug/decimal seed type (#8)
    * Added convert_number_type method to impl.py

--- dbt/adapters/azuresynapse/impl.py
+++ dbt/adapters/azuresynapse/impl.py
@@ -1,3 +1,4 @@
+import agate
 from dbt.adapters.azuresynapse import AzureSynapseConnectionManager
 from dbt.adapters.sql import SQLAdapter
 
@@ -6,9 +7,14 @@ class AzureSynapseAdapter(SQLAdapter):
     ConnectionManager = AzureSynapseConnectionManager
 
     @classmethod
-    def date_function(cls):
+    def date_function(cls) -> str:
         return "get_date()"
 
     @classmethod
-    def convert_text_type(cls, agate_table, col_idx):
+    def convert_text_type(cls, agate_table: agate.Table, col_idx: int) -> str:
         return "varchar(8000)"
+
+    @classmethod
+    def convert_number_type(cls, agate_table: agate.Table, col_idx: int) -> str:
+        decimals = agate_table.aggregate(agate.MaxPrecision(col_idx))
+        return "float" if decimals else "integer"
